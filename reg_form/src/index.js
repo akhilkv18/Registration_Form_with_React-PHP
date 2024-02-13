c4940c61f4c1fbd11ae06c2afac0ae13127a59be
akhil18 <akhilkvmcakuc@gmail.com>
2024-02-13 15:55:15 +0530

    Handled the rendering in index.js with the components from form.js and view.js

--- reg_form/src/index.js
+++ reg_form/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import First from './StudentRegForm/form';
+import Second from './StudentRegForm/view';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  // State to hold the submitted user data
+  const [userData, setUserData] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      {/* Conditional rendering: Render First component if userData is null, else render Second component */}
+      {!userData ? <First setUserData={setUserData} /> : <Second userData={userData} />}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
